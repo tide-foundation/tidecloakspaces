d61270c96c2013eb2268afdcb92d77f0216d19ae
Dom <dom@tide.org>
2025-03-27 16:17:17 +1000

    localhost removed

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -27,38 +27,36 @@ docker run -d \
   -e KC_BOOTSTRAP_ADMIN_PASSWORD=password \
   tideorg/tidecloak-dev:latest
 
-TIDECLOAK_LOCAL_URL="http://localhost:8080"
-
 echo "⏳ [6/10] Waiting for Tidecloak to become ready..."
-until curl -s "${TIDECLOAK_LOCAL_URL}/realms/master/.well-known/openid-configuration" > /dev/null; do
+until curl -s "${CODESPACE_URL_TC}/realms/master/.well-known/openid-configuration" > /dev/null; do
   sleep 2
   echo "⌛ Still waiting for Tidecloak..."
 done
 
 echo "🔐 [7/10] Fetching admin token..."
 RESULT=$(curl -s --data "username=admin&password=password&grant_type=password&client_id=admin-cli" \
-  "${TIDECLOAK_LOCAL_URL}/realms/master/protocol/openid-connect/token")
+  "${CODESPACE_URL_TC}/realms/master/protocol/openid-connect/token")
 TOKEN=$(echo "$RESULT" | sed 's/.*access_token":"//g' | sed 's/".*//g')
 
 echo "🌍 [8/10] Creating realm using Admin API..."
-curl -s -X POST "${TIDECLOAK_LOCAL_URL}/admin/realms" \
+curl -s -X POST "${CODESPACE_URL_TC}/admin/realms" \
   -H "Authorization: Bearer $TOKEN" \
   -H "Content-Type: application/json" \
   --data-binary @tidecloak-client-nextJS/test-realm.json
 
 echo "📤 [9/10] Calling setUpTideRealm vendor endpoint..."
-curl -s -X POST "${TIDECLOAK_LOCAL_URL}/admin/realms/nextjs-test/vendorResources/setUpTideRealm" \
+curl -s -X POST "${CODESPACE_URL_TC}/admin/realms/nextjs-test/vendorResources/setUpTideRealm" \
   -H "Authorization: Bearer $TOKEN" \
   -d "email=email@example.com"
 
 echo "📥 [10/10] Fetching adapter config and writing to tidecloak.json..."
 CLIENT_RESULT=$(curl -s -X GET \
-  "${TIDECLOAK_LOCAL_URL}/admin/realms/nextjs-test/clients?clientId=account" \
+  "${CODESPACE_URL_TC}/admin/realms/nextjs-test/clients?clientId=account" \
   -H "Authorization: Bearer $TOKEN")
 CLIENT_UID=$(echo "$CLIENT_RESULT" | jq -r '.[0].id')
 
 ADAPTER_RESULT=$(curl -s -X GET \
-  "${TIDECLOAK_LOCAL_URL}/admin/realms/nextjs-test/vendorResources/get-installations-provider?clientId=$CLIENT_UID&providerId=keycloak-oidc-keycloak-json" \
+  "${CODESPACE_URL_TC}/admin/realms/nextjs-test/vendorResources/get-installations-provider?clientId=$CLIENT_UID&providerId=keycloak-oidc-keycloak-json" \
   -H "Authorization: Bearer $TOKEN")
 
 echo "$ADAPTER_RESULT" > tidecloak-client-nextJS/tidecloak.json
